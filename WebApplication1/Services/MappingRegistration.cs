a2592b9bea525e27675793238a9125d3d96d5c8c
kulagin <alex_kulagin@samgk.ru>
2023-11-15 21:50:32 +0400

    Добавлен файл конфигации мапстера

---
+++ WebApplication1/Services/MappingRegistration.cs
@@ -0,0 +1,12 @@
+using Mapster;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services;
+
+public class MappingRegistration : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+
+    }
+}
