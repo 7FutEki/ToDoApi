10179a30c8382eddf2107f89a2b360e47a03dbd0
kulagin <alex_kulagin@samgk.ru>
2023-11-15 20:38:49 +0400

    DBSet сверху, правила хорошего тона свойства вначале

--- WebApplication1/Data/ApplicationContext.cs
+++ WebApplication1/Data/ApplicationContext.cs
@@ -3,13 +3,14 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Data
 {
-    public class ApplicationContext :DbContext
+    public class ApplicationContext : DbContext
     {
+        public DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source= = ToDoList.db");
         }
-        public DbSet<TodoItem> TodoItems { get; set; }
-        public DbSet<User> Users { get; set; }
     }
 }
