a4f1ea031769e4e64b453ee83b597f5cabf16815
kulagin <alex_kulagin@samgk.ru>
2023-11-15 21:54:36 +0400

    Настройка мапинга

--- WebApplication1/Services/MappingRegistration.cs
+++ WebApplication1/Services/MappingRegistration.cs
@@ -7,6 +7,12 @@ public class MappingRegistration : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-
+        config.NewConfig<TodoItem, RTNToDo>()
+            .Map(z => z.Birthday, x => x.User!.Birthday)
+            .Map(z => z.CreatedDate, x => x.CreatedDate)
+            .Map(z => z.Priority, x => x.Priority)
+            .Map(z => z.Name, x => x.User!.Name)
+            .Map(z => z.Login, x => x.User!.UserName)
+            .Map(z => z.Title, x => x.Name);
     }
 }
